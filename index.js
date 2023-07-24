0ef376438dbc789ac1378942483e8b4df75dc107
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-07-24 07:48:43 +0600

    finding single college through its _id

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { ObjectId } = require('mongodb');
+
 
 //middleware
 app.use(cors())
@@ -15,24 +17,52 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        //await client.connect();
+        // Send a ping to confirm a successful connection
+        //await client.db("admin").command({ ping: 1 });
+
+
+        const CollegesCollection = client.db('AcademiGataDB').collection('CollegesData');
+
+
+
+        // Get all colleges
+        app.get('/colleges', async (req, res) => {
+            const cursor = CollegesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Get a specific college by ID
+        app.get('/colleges/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await CollegesCollection.findOne(query);
+            res.send(result);
+        });
+
+
+
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -41,9 +71,9 @@ run().catch(console.dir);
 // Root endpoint
 app.get('/', (req, res) => {
     res.send("Server is running");
-  });
-  
-  // Start the server
-  app.listen(port, () => {
+});
+
+// Start the server
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+});
